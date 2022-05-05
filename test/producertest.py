37f74f48e212b4cb79d6cd98ec38694696768b25
Devin Bost <dbost@overstock.com>
2022-05-05 14:25:51 -0500

    Got tests passing after migrating to AWS provider

--- test/producertest.py
+++ test/producertest.py
@@ -32,13 +32,13 @@ class TestBulkPulsarProducer(unittest.TestCase):
         print(pulsarProducerMock.call_args)
 
     def test_astraConnection(self):
-        service_url = 'pulsar+ssl://pulsar-gcp-uscentral1.streaming.datastax.com:6651'
+        service_url = 'pulsar+ssl://pulsar-aws-useast1.streaming.datastax.com:6651'
         token = os.getenv('ASTRA_TOKEN_TEST1')
         # Note: To setup env vars, create .env file in root project dir. That's where it gets picked up.
         client = pulsar.Client(service_url,
                                 authentication=pulsar.AuthenticationToken(token))
 
-        producer = client.create_producer('persistent://contoso-corp/default/testme')
+        producer = client.create_producer('persistent://traffic-corp/default/testme')
 
         for i in range(10):
             producer.send(('Hello World! %d' % i).encode('utf-8'))
@@ -46,13 +46,13 @@ class TestBulkPulsarProducer(unittest.TestCase):
         client.close()
 
     def test_astraConnection(self):
-        service_url = 'pulsar+ssl://pulsar-gcp-uscentral1.streaming.datastax.com:6651'
+        service_url = 'pulsar+ssl://pulsar-aws-useast1.streaming.datastax.com:6651'
         token = os.getenv('ASTRA_TOKEN_TEST_SIMPLE')
         # Note: To setup env vars, create .env file in root project dir. That's where it gets picked up.
         client = pulsar.Client(service_url,
                                 authentication=pulsar.AuthenticationToken(token))
 
-        producer = client.create_producer('persistent://contoso-corp/default/testme')
+        producer = client.create_producer('persistent://traffic-corp/default/testme')
 
         for i in range(10):
             producer.send(('Hello World! %d' % i).encode('utf-8'))
@@ -60,14 +60,14 @@ class TestBulkPulsarProducer(unittest.TestCase):
         client.close()
 
     def test_astraConnectionWithSchema(self):
-        service_url = 'pulsar+ssl://pulsar-gcp-uscentral1.streaming.datastax.com:6651'
+        service_url = 'pulsar+ssl://pulsar-aws-useast1.streaming.datastax.com:6651'
         token = os.getenv('ASTRA_TOKEN_TEST_SCHEMA')
         bp = BulkProducer.PulsarBulkProducer(ANY, ANY)
         # Note: To setup env vars, create .env file in root project dir. That's where it gets picked up.
         client = pulsar.Client(service_url,
                                 authentication=pulsar.AuthenticationToken(token))
         mySchema = Incident.getIncidentSchema()
-        producer = client.create_producer('persistent://contoso-corp/default/testme-schema', schema=mySchema)
+        producer = client.create_producer('persistent://traffic-corp/default/testme-schema', schema=mySchema)
         cwd = os.getcwd()
         df = Utils.loadData(cwd + "/test/incident_sample.csv",  'TrafficReportID')
         bp.produceData(producer, df)
@@ -75,10 +75,10 @@ class TestBulkPulsarProducer(unittest.TestCase):
         client.close()
     
     def test_bulkTrafficData(self):
-        service_url = 'pulsar+ssl://pulsar-gcp-uscentral1.streaming.datastax.com:6651'
+        service_url = 'pulsar+ssl://pulsar-aws-useast1.streaming.datastax.com:6651'
         # Note: To setup env vars, create .env file in root project dir. That's where it gets picked up.
         
-        bp = BulkProducer.PulsarBulkProducer('persistent://austin/ingest/traffic-backfill', service_url)
+        bp = BulkProducer.PulsarBulkProducer('persistent://traffic-corp/default/traffic-backfill-schema', service_url)
         bp.main('ASTRA_TOKEN_TEST_BULKLOAD', Incident.getIncidentSchema)
     
 
@@ -108,8 +108,8 @@ class TestRSSPulsarProducer(unittest.TestCase):
         print(feed)
     
     def test_RSSFeedProducer(self):
-        service_url = 'pulsar+ssl://pulsar-gcp-uscentral1.streaming.datastax.com:6651'
+        service_url = 'pulsar+ssl://pulsar-aws-useast1.streaming.datastax.com:6651'
         # Note: To setup env vars, create .env file in root project dir. That's where it gets picked up.
         
-        rssProducer = RSSFeedProducer.PulsarRssFeedProducer('persistent://contoso-corp/default/testme', service_url)
+        rssProducer = RSSFeedProducer.PulsarRssFeedProducer('persistent://traffic-corp/default/testme', service_url)
         rssProducer.main('ASTRA_TOKEN_TEST_SIMPLE', 1.0, 10.0)
